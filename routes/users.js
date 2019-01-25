421f984880ba531c5938e51fa94b851a46e794e7
msjlwhite <msjlwhite@gmail.com>
2019-01-25 09:25:13 -0600

    Push test 1.25.2019

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
+/* 
+    * Author(s): Kiowa Alumno, Troy Peterson, Jo Lee White
+    * Creation Date: 01/16/2019
+    * Date Updated: 01/16/2019
+    * Description: 
+*/
 var express = require('express');
 var router = express.Router();
 
